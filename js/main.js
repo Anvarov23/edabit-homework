f524d0c493df54cdc8f7d7ffa0fd2a95b91b3bb4
Anvarov23 <anvarovjasurbek2003@gmail.com>
2022-06-17 00:15:22 +0500

    edabit tasks

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// ----------------------------- 1 -----------------------------
+// function addition(a, b) {
+// 	return a + b
+// }
+
+// ----------------------------- 2 -----------------------------
+// function convert(minutes) {
+// 	return minutes * 60
+// }
+
+// ----------------------------- 3 -----------------------------
+// function addition(num) {
+// 	return ++num
+// }
+
+// ----------------------------- 4 -----------------------------
+// function triArea(base, height) {
+// 	return (base * height) / 2
+// }
+
+// ----------------------------- 5 -----------------------------
+// function calcAge(age) {
+// 	return age * 365
+// }
+
+// ----------------------------- 6 -----------------------------
+// function remainder(x, y) {
+// 	return x % y
+// }
+
+// ----------------------------- 7 -----------------------------
+// function footballPoints(wins, draws, losses) {
+// 	var w = wins * 3
+// 	var d = draws * 1
+// 	var l = losses * 0
+// 	return w + d + l
+// }
+
+// ----------------------------- 8 -----------------------------
+// function points(twoPointers, threePointers) {
+// 	var double = twoPointers * 2
+// 	var triple = threePointers * 3
+// 	return double + triple
+// }
+
+// ----------------------------- 9 -----------------------------
+// function lessThan100(a, b) {
+// 	if(a + b < 100){
+// 		return true
+// 	}else {
+// 		return false
+// 	}
+
+// ----------------------------- 10 -----------------------------
+// function animals(chickens, cows, pigs) {
+// 	var ch = chickens * 2
+// 	var c = cows * 4
+// 	var p = pigs * 4
+// 	return ch + c + p
+// }
+
